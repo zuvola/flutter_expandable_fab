e1e4e6706cf7757b67cd832122c13a06bb10e8f3
zuvola <zuvola@gmail.com>
2023-07-22 15:16:24 +0900

    Move source file to under src

--- lib/src/action_button_builder.dart
+++ lib/src/action_button_builder.dart
@@ -2,11 +2,12 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-import '../flutter_expandable_fab.dart';
+/// The size of the expanded FAB.
+enum ExpandableFabSize { small, regular, large }
 
 class FloatingActionButtonBuilder {
   const FloatingActionButtonBuilder({
-    this.size = ExpandableFabSize.regular,
+    required this.size,
     required this.builder,
   });
 
@@ -14,38 +15,55 @@ class FloatingActionButtonBuilder {
       Animation<double> progress) builder;
 
   /// The size of the FAB.
-  final ExpandableFabSize size;
+  final double size;
+}
+
+double _actualSize(ExpandableFabSize fabSize) {
+  switch (fabSize) {
+    case ExpandableFabSize.large:
+      return 96.0;
+    case ExpandableFabSize.small:
+      return 40.0;
+    default:
+      return 56.0;
+  }
 }
 
 class DefaultFloatingActionButtonBuilder extends FloatingActionButtonBuilder {
   DefaultFloatingActionButtonBuilder({
-    super.size,
+    this.fabSize = ExpandableFabSize.regular,
     this.foregroundColor,
     this.backgroundColor,
     this.shape,
     this.heroTag,
     this.child,
-  }) : super(builder: (BuildContext context, VoidCallback? onPressed,
-            Animation<double> progress) {
-          var func = FloatingActionButton.small;
-          switch (size) {
-            case ExpandableFabSize.large:
-              func = FloatingActionButton.large;
-              break;
-            case ExpandableFabSize.small:
-              break;
-            default:
-              func = FloatingActionButton.new;
-          }
-          return func.call(
-            foregroundColor: foregroundColor,
-            backgroundColor: backgroundColor,
-            shape: shape,
-            heroTag: heroTag,
-            onPressed: onPressed,
-            child: child,
-          );
-        });
+  }) : super(
+          size: _actualSize(fabSize),
+          builder: (BuildContext context, VoidCallback? onPressed,
+              Animation<double> progress) {
+            var func = FloatingActionButton.small;
+            switch (fabSize) {
+              case ExpandableFabSize.large:
+                func = FloatingActionButton.large;
+                break;
+              case ExpandableFabSize.small:
+                break;
+              default:
+                func = FloatingActionButton.new;
+            }
+            return func.call(
+              foregroundColor: foregroundColor,
+              backgroundColor: backgroundColor,
+              shape: shape,
+              heroTag: heroTag,
+              onPressed: onPressed,
+              child: child,
+            );
+          },
+        );
+
+  /// The size of the FAB.
+  final ExpandableFabSize fabSize;
 
   /// The default foreground color for icons and text within the button.
   final Color? foregroundColor;
@@ -65,32 +83,38 @@ class DefaultFloatingActionButtonBuilder extends FloatingActionButtonBuilder {
 
 class RotateFloatingActionButtonBuilder extends FloatingActionButtonBuilder {
   RotateFloatingActionButtonBuilder({
-    super.size,
+    this.fabSize = ExpandableFabSize.regular,
     this.foregroundColor,
     this.backgroundColor,
     this.shape,
     this.heroTag,
     this.child,
     this.angle = math.pi / 2,
-  }) : super(builder: (BuildContext context, VoidCallback? onPressed,
-            Animation<double> progress) {
-          return AnimatedBuilder(
-            animation: progress,
-            builder: (context, _) {
-              return Transform.rotate(
-                angle: progress.value * angle,
-                child: DefaultFloatingActionButtonBuilder(
-                  foregroundColor: foregroundColor,
-                  backgroundColor: backgroundColor,
-                  shape: shape,
-                  heroTag: heroTag,
-                  size: size,
-                  child: child,
-                ).builder(context, onPressed, progress),
-              );
-            },
-          );
-        });
+  }) : super(
+          size: _actualSize(fabSize),
+          builder: (BuildContext context, VoidCallback? onPressed,
+              Animation<double> progress) {
+            return AnimatedBuilder(
+              animation: progress,
+              builder: (context, _) {
+                return Transform.rotate(
+                  angle: progress.value * angle,
+                  child: DefaultFloatingActionButtonBuilder(
+                    foregroundColor: foregroundColor,
+                    backgroundColor: backgroundColor,
+                    shape: shape,
+                    heroTag: heroTag,
+                    fabSize: fabSize,
+                    child: child,
+                  ).builder(context, onPressed, progress),
+                );
+              },
+            );
+          },
+        );
+
+  /// The size of the FAB.
+  final ExpandableFabSize fabSize;
 
   /// The default foreground color for icons and text within the button.
   final Color? foregroundColor;
